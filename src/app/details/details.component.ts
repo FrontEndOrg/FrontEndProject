aec48d2af25166ea0273765420de5520c8ce481c
LalithaSyamala <2002syamala@gmail.com>
2021-06-10 23:35:01 +0530

    switching

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../data.service';
-
+import {Subscription} from 'rxjs'
 
 @Component({
   selector: 'app-details',
@@ -11,15 +11,19 @@ import { DataService } from '../data.service';
 export class DetailsComponent implements OnInit {
 
   details;
+  mySubscription:Subscription;
 
   constructor(private ar:ActivatedRoute,private ds:DataService) { }
 
   ngOnInit(): void {
+    
     let id=this.ar.snapshot.params.id;
-    this.ds.getFoodDataId(id).subscribe(
+    console.log("id",id)
+    this.mySubscription=this.ds.getFoodDataId(id).subscribe(
       obj=>{
         console.log("obj is ",obj);
-        this.details=obj;0
+        this.details=obj;
+        console.log("obj is ",this.details);
       },
       err=>{
         console.log("err in reading post",err);
